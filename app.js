d757c6d9fbac27e75eaf4dba7640e4831fa422f7
JenriqueGuerrero <jenriqueguerrero@mail.uniatlantico.edu.co>
2025-02-05 21:46:36 -0500

    Se agrego la funcionalidad a la funcion addFriend()

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-friendList = [];
+//Reto del amigo secreto para Alura LATAM;
+//Primero se crea un array (lista) donde se almacenaran los nombres;
+let friend = [];
 
+//Función que será llamada al presionar el botón "addFriend";
 function addFriend(){
+    let getFriend = document.getElementById('nameFriend').value.trim();
+//Primero se verifica que la entrada no esté vacía; y que el nombre no esté ya en la lista;
+
+    if(getFriend !== "" && !friend.includes(getFriend)){
+        friend.push(getFriend);
+    } else {
+        alert("Por favor, ingrese un nombre válido o que no esté repetido");
+    }
     
-}
+};
