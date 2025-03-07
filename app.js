a6162d23b75ba8c27264534f797bc7a7c47e130d
JenriqueGuerrero <jenriqueguerrero@mail.uniatlantico.edu.co>
2025-03-06 22:39:35 -0500

    Creacion de funcion clearInput

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 //Reto del amigo secreto para Alura LATAM;
-//Primero se crea un array (lista) donde se almacenaran los nombres;
-let friend = [];
+//Array (lista) donde se almacenaran los nombres;
+const friend = [];
 
 //Función que será llamada al presionar el botón "addFriend";
-function addFriend(){
+function addFriend (){
     let getFriend = document.getElementById('nameFriend').value.trim();
-//Primero se verifica que la entrada no esté vacía; y que el nombre no esté ya en la lista;
+    //Primero se verifica que la entrada no esté vacía; y que el nombre no esté ya en la lista;
 
     if(getFriend !== "" && !friend.includes(getFriend)){
         friend.push(getFriend);
+//Verificar entradas        
+        console.log(friend)
+        console.log(getFriend)
     } else {
         alert("Por favor, ingrese un nombre válido o que no esté repetido");
     }
-    
+    clearInput()
 };
+
+// Limpiar la entrada
+function clearInput (){
+    document.getElementById('nameFriend').value = '';
+}
